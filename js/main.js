44cf181681b89607711c23850c9e74d13ea9ef33
DenisPank <den3000d.1267@gmail.com>
2021-09-05 17:12:31 +0300

    end layout

--- js/main.js
+++ js/main.js
@@ -23,20 +23,27 @@ var smoothJumpUp = function () {
 
 const form = document.querySelector(".form__btn");
 const formShipped = document.querySelector(".form__shipped");
-form.addEventListener("click", (event) => {
-  const target = event.target;
-  event.preventDefault();
-  formShipped.classList.add("shipped");
-});
+if (form) {
+  form.addEventListener("click", (event) => {
+    const target = event.target;
+    event.preventDefault();
+    formShipped.classList.add("shipped");
+  });
+}
+
 const closeForm = document.querySelector(".close");
 const popapModal = document.querySelector(".popap-modal");
-closeForm.addEventListener("click", (event) => {
-  const target = event.target;
-  popapModal.classList.toggle("visible");
-  body.classList.toggle("hidden");
-});
+if (closeForm) {
+  closeForm.addEventListener("click", (event) => {
+    const target = event.target;
+    popapModal.classList.toggle("visible");
+    body.classList.toggle("hidden");
+  });
+}
+
 const body = document.querySelector("body");
 const popap = document.querySelectorAll(".popap");
+
 for (let i = 0; i < popap.length; i++) {
   popap[i].addEventListener("click", (event) => {
     const target = event.target;
@@ -111,6 +118,19 @@ const commissionSwiper = new Swiper(".commission__swiper", {
     },
   },
 });
+const typeSwiper = new Swiper(".type__swiper", {
+  slidesPerView: 5,
+  spaceBetween: 15,
+  breakpoints: {
+    320: { slidesPerView: 1.2 },
+    480: { slidesPerView: 2 },
+    576: { slidesPerView: 2.1 },
+    768: { slidesPerView: 2.2 },
+    992: { slidesPerView: 2.8 },
+    1200: { slidesPerView: 3.2 },
+    1400: { slidesPerView: 5 },
+  },
+});
 const howPageSwiper = new Swiper(".how-page__swiper", {
   navigation: {
     nextEl: ".how-page__swiper-button-next",
@@ -156,3 +176,56 @@ const howPageSwiper = new Swiper(".how-page__swiper", {
     1400: { slidesPerView: 3 },
   },
 });
+const popapPerson = document.querySelectorAll(".popap__person");
+const personCardWrap = document.querySelector(".person-card-wrap");
+if (popapPerson) {
+  for (let i = 0; i < popapPerson.length; i++) {
+    const personCard = document.querySelector(".person__card-" + i);
+    popapPerson[i].addEventListener("click", (event) => {
+      event.preventDefault();
+      const target = event.target;
+      if (document.documentElement.clientWidth > 576) {
+        personCard.classList.toggle("visible");
+        body.classList.toggle("hidden");
+        personCardWrap.classList.toggle("visible");
+      } else {
+        target.classList.toggle("more");
+      }
+    });
+    document
+      .querySelectorAll(".close-person")
+      [i].addEventListener("click", (event) => {
+        personCard.classList.remove("visible");
+        body.classList.remove("hidden");
+        personCardWrap.classList.remove("visible");
+      });
+  }
+}
+
+const formVote = document.querySelector(".vote__btn-form");
+const formCode = document.querySelector(".vote__form-code");
+const voteFormEmail = document.querySelector(".vote__form-email");
+if (formVote) {
+  formVote.addEventListener("click", (event) => {
+    const target = event.target;
+    event.preventDefault();
+    voteFormEmail.classList.add("shipped");
+  });
+}
+
+let code = /[0-9]/;
+let inputCode = document.querySelector(".vote__code");
+let notCode = document.querySelector(".notvalide");
+document.querySelector(".vote__bnt-code").onclick = function (e) {
+  e.preventDefault();
+  if (!validate(code, inputCode.value)) {
+    notCode.classList.add("visible");
+  } else {
+    notCode.classList.remove("visible");
+    formShipped.classList.add("shipped");
+    formCode.style.display = "none";
+  }
+};
+function validate(regex, inputCode) {
+  return regex.test(inputCode);
+}
